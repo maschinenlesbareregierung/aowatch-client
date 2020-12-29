e183dc1f16acacb5d1c424a3ab070c1418b0749f
Sebastian Schürmann <maschinenlesbareregierung@outlook.com>
2020-12-29 13:30:27 +0100

    fix: add missing files for sidejobs

---
+++ src/entities/entity.sidejob.ts
@@ -0,0 +1,43 @@
+const axios = require('axios').default;
+import { createRequestQuery } from '../create-request-query';
+import { 
+    EntitySidejob, 
+    ResponseMeta, 
+    ResponseEntityMeta, 
+    PagerParameters, 
+    RangeParameters, 
+    SortParameters, 
+    FilterParameters, 
+    OperatorFilterParameters
+} from '../types';
+export const url = 'https://www.abgeordnetenwatch.de/api/v2/vote'
+
+
+export type SidejobListResult = {
+    meta: ResponseMeta,
+    data: EntitySidejob[]
+}
+
+export type SidejobResult = {
+    meta: ResponseEntityMeta,
+    data: EntitySidejob
+}
+
+export const sidejobList = async (params?: PagerParameters|RangeParameters|null, sort?: SortParameters | null, filter?: FilterParameters | OperatorFilterParameters[]): Promise<SidejobListResult> =>{
+
+    const query = createRequestQuery(params, sort, filter);    
+    const requesturl = !!query ? `${url}?${query}` : url;
+
+    return axios.get(requesturl)
+        .then((response:any)=>response.data)
+        .then((response:any)=>response as SidejobListResult)      
+};
+
+export const sidejob = async (id: number): Promise<SidejobResult> =>{
+    
+    let requestUrl = new URL(`${url}/${id}`);
+
+    return axios.get(requestUrl.toString())
+        .then((response:any)=>response.data)
+        .then((response:any)=>response as SidejobResult)      
+};
