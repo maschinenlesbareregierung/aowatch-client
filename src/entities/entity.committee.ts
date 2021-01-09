f380054d47138e70d8d2bde472aa24fadc7313a6
Sebastian Schürmann <maschinenlesbareregierung@outlook.com>
2021-01-09 18:05:35 +0100

    docs: Committee apidocs

--- src/entities/entity.committee.ts
+++ src/entities/entity.committee.ts
@@ -40,6 +40,15 @@ export type CommitteeResult = {
 
 export type CommitteeRelatedDataParameter = 'show_information' | 'committee_memberships' | 'polls';
 
+/**
+ * Get a list of Committees
+ * ```typescript
+ * response = await committeeList();
+ * ```
+ * @param params  PagerParameters for Paging, RangeParameters for  limiting the results or null
+ * @param sort  Sort simply by a property or more complex by a list of properties
+ * @returns CommitteeListResult as JSON
+ */
 export const committeeList = async (params?: PagerParameters|RangeParameters|null, sort?: SortParameters | null, filter?: FilterParameters | OperatorFilterParameters[]): Promise<CommitteeListResult> =>{
 
     const query = createRequestQuery(params, sort, filter);    
@@ -50,6 +59,15 @@ export const committeeList = async (params?: PagerParameters|RangeParameters|nul
         .then((response:any)=>response as CommitteeListResult)      
 };
 
+/**
+ * Get a single Committee
+ * ```typescript
+ * response = response = await committee(5);
+ * ```
+ * @param id  Id of the Committee.
+ * @param relatedData Possible related Data you can include in the result
+ * @returns CommitteeResult as JSON
+ */
 export const committee = async (id: number, relatedData:CommitteeRelatedDataParameter|null=null): Promise<CommitteeResult> =>{
     
     let requestUrl = new URL(`${url}/${id}`);
