df7d4ee24af085cd9179c958dffc6746c89f2bd1
Sebastian Schürmann <maschinenlesbareregierung@outlook.com>
2021-01-16 01:24:12 +0100

    feature: add way to load external links from profile pages

---
+++ __test__/extract-links.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { extractLinks } from '../src/extract-links';
+import { readFileSync } from 'fs';
+import { parse } from 'url';
+
+import  nock from 'nock';
+
+const testPage = 'https://www.abgeordnetenwatch.de/profile/angela-merkel'
+describe("extract links", ()=>{
+    before(async ()=>{
+        const result = readFileSync('./__test__/fixtures/pages/angela-merkel.html');
+
+        const parsed = parse(testPage)
+        const path = `${parsed.path}`;
+        const baseUrl = `${parsed.protocol}//${parsed.host}`;
+
+        nock(baseUrl)
+            .get(path)
+            .reply(200, result);
+        
+    });
+    it("extract links", async ()=>{ 
+;
+        const res = await extractLinks(testPage);
+        expect(res.length).to.eq(5)
+    });
+});
---
+++ src/extract-links.ts
@@ -0,0 +1,14 @@
+import {EntityPolitician } from './types';
+const axios = require('axios').default;
+const cheerio = require("cheerio");
+
+export const extractLinks = async (url: string)=>{
+    const html = await axios.get(url).then((response:any)=>response.data)    
+    const selector = cheerio.load(html);
+    const searchResults = selector("ul.arrow-list--links > li").find("a");
+    const res: any[] = [];
+    searchResults.map((index: any, data: any)=>{
+        res.push(data.attribs.href)
+    })
+    return res.filter((url:string)=>url.match('http'));  
+};
